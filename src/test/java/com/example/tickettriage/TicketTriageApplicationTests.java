fc8f5cdccaebdde183be8579bb657652b1ccc4f5
Darkmanltu <darkermec@gmail.com>
2026-06-18 14:39:29 +0300

    Initial commit

---
+++ src/test/java/com/example/tickettriage/TicketTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tickettriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
